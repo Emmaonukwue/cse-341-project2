9f642d024e5c88ec8b26284ef38183ebbf9b7001
Emmaonukwue <onukwueemma@gmail.com>
2025-12-01 13:40:31 +0100

    edited swagger.json

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Developers Api',
         description: 'Developers Api'
     },
-    host: 'https://cse-341-project2-01q7.onrender.com/',
+    host: 'cse-341-project2-01q7.onrender.com/',
     schemes: ['http', 'https']
 };
 
