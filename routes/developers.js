3734702d4b5ca83072dea2a7c6054b6773e096fc
Emmaonukwue <onukwueemma@gmail.com>
2025-12-11 13:41:30 +0100

    authentication added

--- routes/developers.js
+++ routes/developers.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const developersController = require('../controllers/developers');
 
+const { isAuthenticated } = require('../middleware/authenticate');
+
 router.get('/', developersController.getAll);
 router.get('/:id', developersController.getSingle);
-
-router.post('/', developersController.createDeveloper);
-router.put('/:id', developersController.updateDeveloper);
-
-router.delete('/:id', developersController.deleteDeveloper);
+router.post('/', isAuthenticated, developersController.createDeveloper);
+router.put('/:id', isAuthenticated, developersController.updateDeveloper);
+router.delete('/:id', isAuthenticated, developersController.deleteDeveloper);
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
+const passport = require('passport');
+
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {
-    //#swagger.tags=['Hello World']
-    res.send('Hello World!');
-});
 
 router.use('/developers', require('./developers'));
 
 router.use('/locations', require('./locations'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
 
